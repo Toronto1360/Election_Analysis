35bd88e819343fb0974e37d16fd0908ec4e45001
toronto1360 <nima.mahdavian@gmail.com>
2021-01-17 23:59:56 -0500

    module 3 challenge complete

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Election_Analysis
-Module 3 Assignment
+## Project Overview
+A colorado Board of Election has given the following tasks to complete an election audit for a recent local congressuonal election.
+1. Calculate the total number of votes cast
+2. Get a complete list of candidates who recieve votes
+3. Calculate the total number of votes each candidate recieved
+4. Calculate the percentage of votes each candidate won.
+5. Determine the winner of the election based on popular vote.
+
+## Resources
+-Data Source: election_results.csv
+-Software: Python 3.7, Visual Studio code
+
+## Summary 
+The analysis of the election show that:
+- The candidates were:
+      Charles Casper Stockham
+      Diana DeGette
+      Raymon Anthony Doane
+ -The Candidate results were:
+      Charles Casper Stockham recieved 23.0% of the vote and 85,213 number of votes.
+      Diana DeGette recieved 73.8% of the vote and 272,892 number of votes.
+      Raymon Anthony Doane recieved 3.1% of the vote and 11,606 number of votes.
+- The winner of the election was:
+      Charles Casper Stockham, who recieved 23.0% of the vote and 85,213 number of votes.
+
+## Challenge Overview
+
+In this challenge along with the analysis performed on the candidate in the election result dataset, we perfomred the analysis on election reslut per county.
+
+## Challenge Summary
+
+Based on our analysis there are overall 369,711 votes in this dataset. Denver with 82.8% of the total votes, holds the majority of votes amongst the three counties. Arapahoe with 6.7% of total votes is the county with the minimum number of votes.
+The winner of this election based on our findings is Diana DeGette with 272,892 votes that holds 73.8% of the total votes recorded for the three counties.
---
+++ pypoll.py
@@ -0,0 +1,90 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+#initialize a total vote counter
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    # Read the header row.
+    headers = next(file_reader)
+
+    #Print each row in the CSV file.
+    for row in file_reader:
+        #Add total vote count
+        total_votes+= 1
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+        #if candidate name is not in candidate options
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            #Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+#Save results into text file.
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+        # Print the candidate list.    
+        #print(candidate_votes)
+        # Determine the percentage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # 4. Print the candidate name and percentage of votes.
+        #print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
+        # Determine winning vote count and candidate
+        # Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # 3. Set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name      
+        # To do: print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")   
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    # Save the winning candidate's name to the text file.
+    txt_file.write(winning_candidate_summary)
+    #print(winning_candidate_summary)
+
+
+    #1. Total number of votes cast
+    #2. A complete list of candidates who received votes
+    #3. Total number of votes each candidate received
+    #4. Percentage of votes each candidate won
+    #5. The winner of the election based on popular vote
